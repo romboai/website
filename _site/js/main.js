0625bf90ea707620665faa597e8dcba902f9ed31
StefanoSalidu <st.salidu@gmail.com>
2021-06-01 13:03:36 +0200

    feat: added animated scrolling to links

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,11 +2,18 @@
   "use strict";
   
  // menu 
-  $('.siteBar-btn').click( function (){ 
-    $('.mobile-menu').toggleClass('siteBar');   
-  }); 
+$('.siteBar-btn').click( function (){ 
+  $('.mobile-menu').toggleClass('siteBar');   
+}); 
 
- 
- 
+$('.sliding-link').click(function(e) {
+  e.preventDefault();
+  var aid = $(this).attr('href');
+  $('html,body').animate({scrollTop: $(aid).offset().top}, 'slow');
+})
  
 })(jQuery);
+
+
+
+

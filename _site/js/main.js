5adc04e910185350891be9dd993c8f217d6a4533
StefanoSalidu <st.salidu@gmail.com>
2021-06-01 14:23:56 +0200

    feat: go back to page

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,16 +1,58 @@
+const SITEBAR_BTN = $('.siteBar-btn');
+const SLIDING_LINK = $('.sliding-link');
+const UP_BTN = $('#UpBtn');
+
+function showButton() {
+  var view = $(window),
+      timeoutKey = -1;
+  
+  $(document).on('scroll', function(e) {
+    e.preventDefault();
+    if (timeoutKey) {
+      window.clearTimeout(timeoutKey);
+    }
+    timeoutKey = window.setTimeout(function(){
+      if (view.scrollTop() < 300) {
+        UP_BTN.fadeOut({
+            duration: 100,
+        });
+        UP_BTN.addClass('d-none');
+      }
+      else {
+        UP_BTN.fadeIn({
+            duration: 100,
+        });
+        UP_BTN.removeClass('d-none');
+      }
+    }, 30);
+  });
+}
+
 (function($) {
   "use strict";
   
  // menu 
-$('.siteBar-btn').click( function (){ 
+ SITEBAR_BTN.click( function (){ 
   $('.mobile-menu').toggleClass('siteBar');   
 }); 
 
-$('.sliding-link').click(function(e) {
+SLIDING_LINK.click(function(e) {
   e.preventDefault();
   var aid = $(this).attr('href');
-  $('html,body').animate({scrollTop: $(aid).offset().top}, 'slow');
+  if (aid) {
+      $('html,body').animate({scrollTop: $(aid).offset().top}, 'slow');
+  }
 })
+
+showButton();
+
+UP_BTN.click(function(e) {
+  e.preventDefault();
+  $('html, body').stop().animate({
+      scrollTop: 0
+  }, 300, 'linear');
+  return false;
+});
  
 })(jQuery);
 
